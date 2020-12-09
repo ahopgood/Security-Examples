886284a552ff804a1478f0ead850b7c8c273a44b
Alex Hopgood <alexhopgood@notonthehighstreet.com>
2020-12-09 11:05:38 +0000

    Adding retrieval and mapping of thumbnails to the bike service implementation.

--- PersistentXSS/src/main/java/com/alexander/security/examples/persistent/xss/service/BikeServiceImpl.java
+++ PersistentXSS/src/main/java/com/alexander/security/examples/persistent/xss/service/BikeServiceImpl.java
@@ -1,11 +1,29 @@
 package com.alexander.security.examples.persistent.xss.service;
 
+import com.alexander.security.examples.persistent.xss.Mapper;
+import com.alexander.security.examples.persistent.xss.persistence.model.BikeThumbnailEntity;
+import com.alexander.security.examples.persistent.xss.persistence.repository.BikeThumbnailRepository;
 import com.alexander.security.examples.persistent.xss.service.model.BikeThumbnail;
 import com.alexander.security.examples.persistent.xss.service.model.BikeDetails;
+import org.springframework.stereotype.Service;
 
+import javax.inject.Inject;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class BikeServiceImpl implements BikeService {
+
+    private final BikeThumbnailRepository bikeThumbnailRepository;
+    private final Mapper<BikeThumbnail, BikeThumbnailEntity> bikeThumbnailMapper;
+
+    @Inject
+    public BikeServiceImpl(BikeThumbnailRepository bikeThumbnailRepository,
+                           Mapper<BikeThumbnail, BikeThumbnailEntity> bikeThumbnailMapper) {
+        this.bikeThumbnailRepository = bikeThumbnailRepository;
+        this.bikeThumbnailMapper = bikeThumbnailMapper;
+    }
+
     @Override
     public BikeDetails getBikeDetails(String id) {
         return null;
@@ -13,6 +31,8 @@ public class BikeServiceImpl implements BikeService {
 
     @Override
     public List<BikeThumbnail> getBikeThumbnails() {
-        return null;
+        return bikeThumbnailRepository.getBikeThumbnails().stream()
+                .map(bikeThumbnailMapper::map)
+                .collect(Collectors.toList());
     }
 }
