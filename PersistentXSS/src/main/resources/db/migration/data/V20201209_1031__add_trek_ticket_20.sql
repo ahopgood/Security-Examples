da47872cba67339c3def67c507e2a081f0585116
Alexander Hopgood <alex.hopgood@gmail.com>
2020-12-14 07:55:34 +0000

    Adding another comment to the trek ticket 20 migration to assist with testing the database row mapper

--- PersistentXSS/src/main/resources/db/migration/data/V20201209_1031__add_trek_ticket_20.sql
+++ PersistentXSS/src/main/resources/db/migration/data/V20201209_1031__add_trek_ticket_20.sql
@@ -7,5 +7,9 @@ INSERT INTO thumbnails (id, bike_id, url) VALUES (@thumbnail_id, @trek_id, 'thum
 SET @comment_id='c1';
 INSERT INTO comments (id, bike_id, comment) VALUES (@comment_id, @trek_id, 'I rode this bike until it broke');
 
+SET @comment_id='c2';
+INSERT INTO comments (id, bike_id, comment) VALUES (@comment_id, @trek_id, 'I bought this for my paper round');
+
 SET @bike_detail_id='d1';
-INSERT INTO bike_details (id, bike_id, url, description) VALUES (@bike_detail_id, @trek_id, 'large/trekticket20.png', 'A hard tail bike perfect for trails')
+INSERT INTO bike_details (id, bike_id, url, description) VALUES (@bike_detail_id, @trek_id, 'large/trekticket20.png', 'A hard tail bike perfect for trails');
+
