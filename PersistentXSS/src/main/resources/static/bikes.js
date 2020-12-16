b1873623ab657b985769348f4ff6e2b64e26e2c9
Alex Hopgood <alexhopgood@notonthehighstreet.com>
2020-12-16 11:39:59 +0000

    Adding event binding for submitting comments and reloading the bike details to show new comment. Commented out console logging to keep things clean.

--- PersistentXSS/src/main/resources/static/bikes.js
+++ PersistentXSS/src/main/resources/static/bikes.js
@@ -3,16 +3,41 @@ $(document).ready(function() {
     createThumbnails();
 });
 var host = "http://localhost:8080/"
-function populateDetails(event) {
-    console.log("Details for:");
-    console.log(event);
-    console.log(event.data);
+
+function submitComment(event) {
+    var bikeId = event.data;
+//    console.log("I'm a submit comment click event for " + bikeId);
+    var commentContent = $("#commentTextArea").val();
+//    console.log(commentContent);
     jQuery.ajax({
-        url: host + "bikes/detail/" + event.data,
+            url: host + "bikes/" + bikeId + "/comments/",
+            type: "POST",
+            contentType: "application/json",
+            dataType: "json",
+            data: "{\"comment\":\"" + commentContent + "\"}",
+            statusCode: {
+                200: function() {
+//                    console.log("Call completed for " + bikeId);
+                    var loadEvent = jQuery.Event( "madeup" );
+                    loadEvent.data = bikeId;
+                    $("#commentSubmit").off("click");
+                    populateDetails(loadEvent);
+                }
+            }
+        });
+}
+
+function populateDetails(event) {
+//    console.log("Details for:");
+//    console.log(event);
+//    console.log(event.data);
+    var bikeId = event.data;
+    jQuery.ajax({
+        url: host + "bikes/detail/" + bikeId,
         type: "GET",
         dataType: "json",
     }).done(function (json) {
-        console.log(json);
+//        console.log(json);
         var details = json;
         $("#details-template #details-header").html(details.title);
         $("#details-template div img").attr("src", host + details.fullImageUrl);
@@ -22,15 +47,17 @@ function populateDetails(event) {
         var commentTemplate = '<li class="list-group-item"></li>';
         jQuery.each( details.comments, function( index, comment ){
             var commentTemplate = comment;
-            console.log(comment);
+//            console.log(comment);
             $("ul.list-group").append("<li class=\"list-group-item\"></li>")
             $("ul.list-group li.list-group-item:last").html(comment.comment);
         });
     }); //ajax end
+
+    //Setup comment submit actions
+    $("#commentSubmit").click(bikeId, submitComment);
 }
 
 function createThumbnails() {
-    var host = "http://localhost:8080/"
 //    console.log($("body"));
 
     jQuery.ajax({
@@ -39,7 +66,7 @@ function createThumbnails() {
         dataType: "json",
     }).done(function (json) {
         jQuery.each( json, function( index, json ){
-            console.log(json);
+//            console.log(json);
             var thumbnail = json;
             var bikeId = thumbnail.bikeId;
             var template = $("#template").clone().attr("id", bikeId).toggleClass("invisible")
