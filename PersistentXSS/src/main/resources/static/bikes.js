6397be1a7b439c1c718fd1ccb366ce42adb3ba46
Alex Hopgood <alexhopgood@notonthehighstreet.com>
2021-01-06 09:22:05 +0000

    Updating visibility modifiers for hidden card template so it doesn't take up space in the layout. Updated visibility for initial display of the bike details card so it doesn't show empty card until details are requested and populated.

--- PersistentXSS/src/main/resources/static/bikes.js
+++ PersistentXSS/src/main/resources/static/bikes.js
@@ -39,6 +39,7 @@ function populateDetails(event) {
     }).done(function (json) {
 //        console.log(json);
         var details = json;
+        $("#details").removeClass("d-none");
         $("#details-template #details-header").html(details.title);
         $("#details-template div img").attr("src", host + details.fullImageUrl);
         $("#details-template .card-text").html(details.bikeDescription);
@@ -69,7 +70,7 @@ function createThumbnails() {
 //            console.log(json);
             var thumbnail = json;
             var bikeId = thumbnail.bikeId;
-            var template = $("#template").clone().attr("id", bikeId).toggleClass("invisible")
+            var template = $("#template").clone().attr("id", bikeId).toggleClass("d-none");
             $("body div.container .card-group.row").append(template);
             $("#" + bikeId + " img").attr("src", host + thumbnail.thumbnailImageUrl);
             $("#" + bikeId + " div.card-body h5").html(thumbnail.title);
