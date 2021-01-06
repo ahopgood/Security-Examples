19ab0b590f739bc2195f05f4e984d51baa6050c0
Alex Hopgood <alexhopgood@notonthehighstreet.com>
2021-01-06 10:36:47 +0000

    Adding optional support to the BikeDetailsRepository for cases where details cannot be found for an id.

--- PersistentXSS/src/main/java/com/alexander/security/examples/persistent/xss/persistence/repository/BikeDetailsRepository.java
+++ PersistentXSS/src/main/java/com/alexander/security/examples/persistent/xss/persistence/repository/BikeDetailsRepository.java
@@ -2,9 +2,11 @@ package com.alexander.security.examples.persistent.xss.persistence.repository;
 
 import com.alexander.security.examples.persistent.xss.persistence.model.BikeDetailsEntity;
 
+import java.util.Optional;
+
 public interface BikeDetailsRepository {
 
-    BikeDetailsEntity getBikeDetails(String bikeId);
+    Optional<BikeDetailsEntity> getBikeDetails(String bikeId);
 
     int addComment(String bikeId, String comment);
 }
